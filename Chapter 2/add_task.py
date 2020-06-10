4d5bc690d7c21b67a59e52c9e66ebb4b1ecb8e41
Marcin-99 <MarcinAszyk@o2.pl>
2020-06-10 23:04:34 +0200

    Updated add function

--- Chapter 2/add_task.py
+++ Chapter 2/add_task.py
@@ -6,12 +6,10 @@ def add(name, deadline, description):
     deadline_time_object = datetime.strptime(deadline, '%Y-%m-%d,%H:%M')
 
     session = Session()
-    task = Task()
-
-    task.name = name
-    task.deadline = deadline_time_object
-    task.description = description
-    task.hash = hash(str(name) + str(deadline) + str(description))
+    task = Task(name=name,
+                deadline=deadline_time_object,
+                description=description,
+                hash = hash(str(name) + str(deadline) + str(description)))
 
     session.add(task)
     session.commit()
